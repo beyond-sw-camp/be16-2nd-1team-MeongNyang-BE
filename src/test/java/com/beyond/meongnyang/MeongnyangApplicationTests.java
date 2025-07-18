4a5986d527d30c0a5050b54250b6053988d2438b
publicguard_ <100781721+YoonSuOh@users.noreply.github.com>
2025-07-18 10:02:38 +0900

    프로젝트 생성

---
+++ src/test/java/com/beyond/meongnyang/MeongnyangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyond.meongnyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeongnyangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
