c18a27a61516bdc5040d423f257e751867ffdb61
FOJF <john9823@naver.com>
2025-07-21 19:18:42 +0900

    add alarm entity

---
+++ src/main/java/com/beyond/meongnyang/domain/alarm/entity/Alarm.java
@@ -0,0 +1,33 @@
+package com.beyond.meongnyang.domain.alarm.entity;
+
+import com.beyond.meongnyang.common.entity.CommonAt;
+import com.beyond.meongnyang.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Alarm extends CommonAt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private String content;
+    @Enumerated(EnumType.STRING)
+    private AlarmType alarmType;
+    private Long alarmId;
+}
