33a27b2242b047d328c93d17bd2c5b4b016da6c3
imujinu <wlsdnrhdwkd@gmail.com>
2025-07-22 18:04:41 +0900

    feat/group

---
+++ src/main/java/com/beyond/meongnyang/group/domain/Group.java
@@ -0,0 +1,12 @@
+package com.beyond.meongnyang.group.domain;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Group {
+}
---
+++ src/main/java/com/beyond/meongnyang/group/dto/req/GroupCreateReqDto.java
@@ -0,0 +1,11 @@
+package com.beyond.meongnyang.group.dto.req;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupCreateReqDto {
+}
