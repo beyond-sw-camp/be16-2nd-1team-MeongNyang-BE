c5c9cb96554dd98f31723ae44cfd9ca4090b69a9
FOJF <john9823@naver.com>
2025-07-22 18:43:10 +0900

    테이블명, 컬럼명 명시

--- src/main/java/com/beyond/meongnyang/alarm/entity/Alarm.java
+++ src/main/java/com/beyond/meongnyang/alarm/entity/Alarm.java
@@ -13,9 +13,11 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "alarm")
 public class Alarm extends CommonAt {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -26,8 +28,11 @@ public class Alarm extends CommonAt {
     @JoinColumn(name = "receiver_id")
     private User receiver;
 
+    @Column(name = "content")
     private String content;
     @Enumerated(EnumType.STRING)
+    @Column(name = "alarm_type")
     private AlarmType alarmType;
+    @Column(name = "alarm_id")
     private Long alarmId;
 }
