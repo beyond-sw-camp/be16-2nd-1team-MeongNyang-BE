079449ec11719c91a92fb11ec33ec023c46ca9a2
FOJF <john9823@naver.com>
2025-09-10 00:46:27 +0900

    rollback : 불필요한 환경 세팅 제거

--- src/main/java/com/beyond/meongnyang/MeongnyangApplication.java
+++ src/main/java/com/beyond/meongnyang/MeongnyangApplication.java
@@ -1,6 +1,5 @@
 package com.beyond.meongnyang;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +7,8 @@ import java.util.TimeZone;
 
 @SpringBootApplication
 public class MeongnyangApplication {
-	@PostConstruct
-	public void started() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-	}
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(MeongnyangApplication.class, args);
 	}
 
